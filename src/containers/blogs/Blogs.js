1f04788a12fa1d495745696a5ec3e297ce49ea94
gwilldan <sayhi2godswill@gmail.com>
2023-02-13 15:57:51 +0100

    added blog

--- src/containers/blogs/Blogs.js
+++ src/containers/blogs/Blogs.js
@@ -5,7 +5,7 @@ function Blogs(props) {
     return (
         <div className='gpt3__blogs section__padding'>
             <h3 className='gpt3__header-hero_text_h1 gradient__text'>A lot is happening, <br /> We are blogging about it.</h3>
-
+            <p> I AM THE NEW BLOG</p>
         </div>
     );
 }
